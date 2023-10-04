f326c463d76984ae40044c832f600eb969d3be05
Ddreyy <mradedapodada@gmail.com>
2023-10-04 20:07:42 +0100

    changes made

--- src/App.js
+++ src/App.js
@@ -1,95 +1,76 @@
-import React, { useEffect, useState } from 'react';
-
-import useHelloWorld from './custom-hooks/useHelloWorld.js';
-
-import './style.css';
-
-const months = {
-  January: 31,
-  February: 28,
-  March: 31,
-  April: 30,
-  May: 31,
-  June: 30,
-  July: 31,
-  August: 31,
-  September: 30,
-  October: 31,
-  November: 30,
-  December: 31,
-};
-
+import React, { useState } from "react";
 
 const useDate = () => {
-  const date = new Date();
+  const currentDate = new Date();
 
   const getDay = () => {
-    return date.getDay();
+    return currentDate.getDate();
   };
 
   const getMonth = () => {
-    return date.getMonth();
+    return currentDate.getMonth();
   };
 
   const addDay = (numberOfDays) => {
-    //N.B if day after adding is greater than number of days for that month, date returned should be a new month date
-    const result = date.getDay() + numberOfDays;
-    return result
-   
+    const newDate = new Date(currentDate);
+    newDate.setDate(newDate.getDate() + numberOfDays);
+    return newDate;
   };
 
   const addMonth = (numberOfMonths) => {
-    //N.B if month after adding is greater than 11, date returned should be a new year
-    return date.getMonth() + numberOfMonths;
+    const newDate = new Date(currentDate);
+    newDate.setMonth(newDate.getMonth() + numberOfMonths);
+    return newDate;
   };
 
-  return { date, getDay, getMonth, addDay, addMonth };
+  return { currentDate, getDay, getMonth, addDay, addMonth };
 };
 
 export default function App() {
-
-  const { date, getDay, getMonth, addDay, addMonth } = useDate();
-  const [newDate , setNewDate] = useState(date)
-  const [newMonth , setNewMonth] = useState(date)
+  const { currentDate, getDay, getMonth, addDay, addMonth } = useDate();
+  const [newDate, setNewDate] = useState(currentDate);
+  const [newMonth, setNewMonth] = useState(currentDate);
 
   const handleAddDay = () => {
-    setNewDate(addDay(20))
-    if(addDay(20) > months.October){
-      console.log("it's a new month cause it exceeds the number of days for the specified month ")
-    }else{
-      console.log("this is still the current month with " ,addDay(20) , "of days ")
+    const updatedDate = addDay(20);
+    setNewDate(updatedDate);
+    if (updatedDate.getMonth() !== currentDate.getMonth()) {
+      console.log("Wow, it's a brand new month! Time flies!");
+    } else {
+      console.log(`The month is still ongoing with ${updatedDate.getDate()} days`);
     }
-  }
+  };
+
   const handleAddMonth = () => {
-    setNewMonth(addMonth(3))
-    if(addMonth(3) > Object.keys(months).length){
-      console.log("a new year")
-    }else{
-      console.log(getMonth())
+    const updatedMonth = addMonth(3);
+    setNewMonth(updatedMonth);
+    if (updatedMonth.getFullYear() !== currentDate.getFullYear()) {
+      console.log("It's a new year");
+    } else {
+      console.log(`Current Month: ${updatedMonth.getMonth()}`);
     }
-  }
+  };
+
 
 
   return (
     <div>
-      <div>
-        <h1>Current Date</h1>
-        <p>Date: {date.toString()}</p>
-        <p>Day: {getDay()}</p>
-        <p>Month: {getMonth()}</p>
+      <h1>Current Date</h1>
+      <p>Date: {currentDate.toString()}</p>
+      <p>Day: {getDay()}</p>
+      <p>Month: {getMonth()}</p>
 
-        <h1>New Date</h1>
-        <p>Days: {date.toString()}</p>
-        
-        <p>Day: {getDay()}</p>
-        <p>Month: {getMonth()}</p>
+      <h1>New Date</h1>
+      <p>Date: {newDate.toString()}</p>
+      <p>Day: {newDate.getDate()}</p>
+      <p>Month: {newDate.getMonth()}</p>
 
-        <p>month: {newDate.toString()}</p>
-        <p>Date: {newMonth.toString()}</p>
+      <h1>New Month</h1>
+      <p>Date: {newMonth.toString()}</p>
+      <p>Month: {newMonth.getMonth()}</p>
 
-        <button onClick = {handleAddDay}>Add days</button>
-        <button onClick = {handleAddMonth}>Add months</button>
-      </div>
+      <button onClick={handleAddDay}>Add days</button>
+      <button onClick={handleAddMonth}>Add months</button>
     </div>
   );
 }
